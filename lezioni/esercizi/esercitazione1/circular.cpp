3be43c1270f6d5f511ad0294ed847953a802da49
ShinMaedaru <segalaedoardo@gmail.com>
2021-02-24 17:57:47 +0100

    commit

---
+++ lezioni/esercizi/esercitazione1/circular.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+/*
+ * Complete the 'getTime' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+long getTime(string s) {
+    int i{0};
+    
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    long result = getTime(s);
+
+
+    fout << s << "\n";
+
+    fout.close();
+
+    return 0;
+}
