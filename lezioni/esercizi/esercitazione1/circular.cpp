b18ce2dd878d787c7a63817bdc074a2c241329a7
ShinMaedaru <segalaedoardo@gmail.com>
2021-02-25 20:58:59 +0100

    sdkvnsvokdsnonvds

--- lezioni/esercizi/esercitazione1/circular.cpp
+++ lezioni/esercizi/esercitazione1/circular.cpp
@@ -11,9 +11,60 @@ using namespace std;
  * The function accepts STRING s as parameter.
  */
 
+int clockWise(char c,string s,int& index){//funziona
+    int i=index;
+    int dim = s.length();
+    int steps = 0;
+  //  char temp; 
+    while(c!=s.at(i)){
+        if(i==dim-1){
+            i=-1;
+        }
+        steps++;
+        ++i;
+        index = i;
+    }
+    return steps;
+}
+
+int counterClockWise(char c,string s,int& index){//funziona
+    int i=index;
+    int dim = s.length();
+    int steps = 0;
+    while(c!=s.at(i)){
+        if (i==0){
+            i=dim;
+        }
+        steps++;
+        --i;
+        index = i;
+    }
+    return steps;
+}
+
+
 long getTime(string s) {
-    int i{0};
-    
+    string s1 = s;
+    string s2 ="abcdefghilmnopqrstuvz";
+    int index1=0;
+    int index2=0;
+    int result;
+    int ccw;
+    int cw;
+    for (char c:s){
+            cw = clockWise(c,s2,index2);
+            ccw =counterClockWise(c,s2,index1);
+        if (cw<ccw){
+            result += cw;
+            index2= index1;
+        }
+        else{
+            result +=  ccw;
+            index1= index2;
+
+        }
+    }
+    return result;
 }
 
 int main()
@@ -26,7 +77,7 @@ int main()
     long result = getTime(s);
 
 
-    fout << s << "\n";
+    fout << result << "\n";
 
     fout.close();
 
