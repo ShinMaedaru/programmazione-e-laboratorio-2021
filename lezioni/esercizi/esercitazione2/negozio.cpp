91cb6d4d02a5ef55e21fb85e4345113bfdc4c49f
ShinMaedaru <segalaedoardo@gmail.com>
2021-03-03 12:43:27 +0100

    hjhù

---
+++ lezioni/esercizi/esercitazione2/negozio.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+/*
+ * Complete the 'featuredProduct' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING_ARRAY products as parameter.
+ */
+bool comparator(string a,string b)
+{
+    return a<b;
+}
+
+string featuredProduct(vector<string> products) {
+
+}
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string products_count_temp;
+    getline(cin, products_count_temp);
+
+    int products_count = stoi(ltrim(rtrim(products_count_temp)));
+
+    vector<string> products(products_count);
+
+    for (int i = 0; i < products_count; i++) {
+        string products_item;
+        getline(cin, products_item);
+
+        products[i] = products_item;
+    }
+
+    string result = featuredProduct(products);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
