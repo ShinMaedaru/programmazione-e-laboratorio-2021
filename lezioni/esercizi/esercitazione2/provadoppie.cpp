c8c45ec10fd246d4b31aa10da3819f4bc5b899a1
ShinMaedaru <segalaedoardo@gmail.com>
2021-03-03 10:59:28 +0100

    biocwfad

---
+++ lezioni/esercizi/esercitazione2/provadoppie.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+
+using std::string;
+
+int main() {
+    string a;
+    getline(std::cin, a);
+    std::cout<<a<<a.length()<<std::endl;
+  
+    
+    
+    return 0;
+}
