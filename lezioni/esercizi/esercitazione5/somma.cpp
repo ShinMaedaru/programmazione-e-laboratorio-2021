c92385340d9bede417c1ea6ce320c81d78775bdd
ShinMaedaru <segalaedoardo@gmail.com>
2021-03-24 19:35:00 +0100

    diocane

---
+++ lezioni/esercizi/esercitazione5/somma.cpp
@@ -0,0 +1,106 @@
+#include<iostream>
+
+
+struct Celladl{
+  int info;
+  Celladl* next;
+  Celladl* prev;
+};
+
+typedef Celladl* Listadl;
+
+void append(Listadl& pt, Listadl& pc, int info) {
+  Listadl nuova = new Celladl;
+  nuova->next = nullptr;
+  nuova->prev = pc;
+  nuova->info = info;
+
+  if (pc) 
+    pc->next = nuova;
+
+  if (!pt) 
+    pt = nuova;
+
+  pc = nuova;
+
+}
+
+void stampa_avanti(Listadl pt) {
+  while (pt) {
+    std::cout<<pt->info<<" ";
+    pt = pt->next;
+  }
+  std::cout << std::endl;
+}
+
+
+void stampa_indietro(Listadl pc) {
+  while (pc) {
+    std::cout << pc->info <<" ";
+    pc = pc->prev;
+  }
+  std::cout << std::endl;
+}
+
+
+void leggi_lista(Listadl& pt, Listadl& pc) {
+   int elements;
+   std::cin >> elements;
+
+   while (elements>0) {
+     int info;
+     std::cin>>info;
+     append(pt, pc, info);
+     elements--;
+   }
+}
+
+void distruggi_lista(Listadl& pt) {
+  Listadl supp;
+  while(pt) {
+    supp = pt;
+    pt = pt->next;
+    delete supp;
+  }
+  pt = nullptr;
+}
+
+
+int somma(Listadl pt, int v) {
+    int temp = -1;
+    
+    while(pt){
+    //casistica testa
+    if(pt->info == v){  
+      if(pt->prev == nullptr){
+          temp = v;
+          temp += pt->next->info;
+      }
+      //casistica coda
+      if(pt->next == nullptr){
+          temp = v;
+          temp += pt->prev->info;
+      }
+      if(pt->next !=nullptr && pt->prev!=nullptr){
+          temp = v;
+          temp += pt->prev->info;
+          temp += pt->next->info;
+      }
+    }      
+      pt=pt->next;
+    }
+    return temp;
+}
+int main() {
+  Listadl pt{nullptr}, pc{nullptr};
+  leggi_lista(pt, pc);
+
+  int find;
+  std::cin>>find;
+
+  std::cout << somma(pt, find); 
+
+  distruggi_lista(pt);
+
+  return 0;
+}
