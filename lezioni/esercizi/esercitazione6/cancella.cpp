80aebd14846775d614c39114ba763e5a1e588242
ShinMaedaru <segalaedoardo@gmail.com>
2021-04-02 15:49:51 +0200

    diocane

---
+++ lezioni/esercizi/esercitazione6/cancella.cpp
@@ -0,0 +1,124 @@
+#include<iostream>
+
+
+struct Cella_circ{
+  int info;
+  struct Cella_circ* next;
+};
+
+
+typedef Cella_circ* Lista_circ;
+
+
+void add(Lista_circ& lista, int x) {
+  Lista_circ nuovo = new Cella_circ;
+  nuovo->info = x;
+  if (lista==nullptr) {
+    nuovo->next = nuovo;
+    lista = nuovo;
+  }
+  else {
+    nuovo->next = lista->next;
+    lista->next = nuovo;
+  }
+}
+
+//stampa lista
+void stampa_lista(Lista_circ l) {
+  if (l) {
+    Lista_circ pc{l};
+    do {
+      std::cout<<l->info << " ";
+      l=l->next;
+    } while (l!=pc);
+  }
+  std::cout<<std::endl;
+}
+
+//distruggi lista
+void distruggi(Lista_circ& l) {
+  if (l) {  
+    Lista_circ supp, sentinella{l};
+    do {
+      supp = l;
+      l=l->next;
+      delete supp;
+    } while (l!=sentinella);
+    l = nullptr;
+  }
+}
+
+
+//leggi lista
+void leggi_lista(Lista_circ& l) {
+  int elem, n;
+  std::cin>>n;
+  while (n>0) {
+    std::cin>>elem;
+    add(l, elem);
+    n--;
+  }
+}
+
+
+
+
+
+void rimuovi_occ(Lista_circ& l, int elem) {
+if(l==nullptr){
+    return;
+  }
+  else{
+     
+    Lista_circ pc=l;
+    Lista_circ del;
+     //caso in cui elemento sia nella coda della lista
+    while(pc!=l->next){
+      if(l->next->info==elem){
+        del=l->next;
+        l->next=l->next->next;
+        delete del;
+      }
+      else
+        l=l->next;
+    }
+      //caso in cui mi rimane solo un nodo nella lista e sia = ad elem
+    if(l==l->next && l->info==elem){
+      Lista_circ temp = l;  
+      l=nullptr;
+      delete(temp);
+    }else{
+     //caso in cui elemento sia testa della lista
+    if(pc->info == elem){
+      l->next = pc->next;
+      l=l->next;
+      delete pc;
+    }
+    else l=pc;
+    }
+  }
+}
+
+    //}
+int main() {
+
+  int elimina;
+  Lista_circ l=nullptr;
+  std::cout<<"dammi la lunghezza e poi i valori"<<std::endl;
+  leggi_lista(l);
+  std::cout<<"il valore da togliere"<<std::endl;
+
+  std::cin>>elimina;
+
+  std::cout<<"Prima: ";
+  stampa_lista(l);
+
+  rimuovi_occ(l, elimina);
+
+  std::cout<<"Dopo: ";
+  stampa_lista(l);
+
+
+  distruggi(l);
+  return 0;
+}
